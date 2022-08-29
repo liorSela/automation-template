9f942ada380637ba7c4cd8e81e669ba88fd63cd1
PEPPERI\evgeny.v <evgeny.v@pepperi.com>
2022-08-29 11:23:27 +0300

    var sk created as empty file as part of init

---
+++ script/setVarSk.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const fs = require("fs");
+
+
+const varSkPath = '../var_sk';
+
+if (!fs.existsSync(varSkPath)) {
+    console.log('no var_sk file - creating');
+    fs.writeFileSync(varSkPath, '');
+}
+
+
+
+
+
